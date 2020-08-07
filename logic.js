badcf63ab5576d24d8f3bdde608c0a597a93a0c4
alyciar <alyciar@secretshopper.com>
2020-08-07 12:38:24 -0500

    this is so buggy

--- logic.js
+++ logic.js
@@ -1,23 +1,3 @@
-//Page loads
-
-//Random word is chosen from array
-
-//blanks that represent the number of letters in word is displayed
-
-//User guesses letter
-
-//If letter is in word, letter appears in place of blank
-
-//if letter is not in word, letter is displayed in "guessed letters" and guesses remaining goes down by 1
-
-//If user correctly guesses word, win counter goes up by 1
-
-//If user runs out of guesses, loss counter goes up by 1
-
-//user is prompted if they want to play again
-
-//Define global variables
-
 var availableWords = [
   "javascript",
   "array",
@@ -57,13 +37,13 @@ function renderGame() {
 
 //Random word is chosen from array
 function generateWord() {
-    document.querySelector("#modal").style.display = "none";
+  document.querySelector("#modal").style.display = "none";
   var randomWord =
     availableWords[Math.floor(Math.random() * availableWords.length)];
   console.log("randomWord: ", randomWord);
   //Split word up into individual characters
-    splitWord = randomWord.split("");
-    guessesRemaining = splitWord.length + 5;
+  splitWord = randomWord.split("");
+  guessesRemaining = splitWord.length + 5;
   //Loop over splitWord and display dashes??
   for (var i = 0; i < splitWord.length; i++) {
     numBlanks.push(" _ ");
@@ -72,44 +52,43 @@ function generateWord() {
 
 //get user input and compare
 document.addEventListener('keydown', function (event) {
-      
+
   guessedLetter = event.key;
   if (splitWord.includes(guessedLetter)) {
     //not pushing multiple instances of variable
     correctGuesses.push(guessedLetter)
     checkWinLose();
     displayCorrectGuesses();
- 
     numCorrect++;
   } else {
     incorrectGuesses.push(guessedLetter);
   }
   guessesRemaining--;
   renderGame();
-  checkWinLose(); 
+  checkWinLose();
 });
 
 //display correct guesses
-function displayCorrectGuesses(){
-  splitWord.forEach(function (item, i) { 
-    var index = item.indexOf(guessedLetter);      
+function displayCorrectGuesses() {
+  splitWord.forEach(function (item, i) {
+    var index = item.indexOf(guessedLetter);
     if (index !== -1) {
-      numBlanks[i] = guessedLetter; 
-      
-  } 
-});
+      numBlanks[i] = guessedLetter;
+    }
+  });
 }
 
 
 //check win/lose
 
-function checkWinLose(){
+function checkWinLose() {
   if (guessesRemaining <= 0) {
     numBlanks = [];
     losses++;
     document.querySelector("#modal").innerHTML = "You lose!";
     document.querySelector("#modal").style.display = "inline-block";
-  } if (numCorrect == numBlanks.length) {
+  }
+  if (numCorrect == numBlanks.length) {
     wins++;
     numBlanks = [];
     document.querySelector("#win-lose-text").innerHTML = "You Win!";
@@ -121,15 +100,14 @@ function checkWinLose(){
 
   }
 }
-  $(document).on("click", ".play-again", function () {
-    guessedLetter = null;  
-    correctGuesses= [];
-    incorrectGuesses = [];    
-    guessesRemaining = [];
-    numCorrect = [];
-    generateWord();
-    renderGame();
-  });
-
+$(document).on("click", ".play-again", function () {
+  guessedLetter = null;
+  correctGuesses = [];
+  incorrectGuesses = [];
+  guessesRemaining = [];
+  numCorrect = [];
   generateWord();
+  renderGame();
+});
 
+generateWord();
